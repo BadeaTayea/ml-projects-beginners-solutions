2b0ef16754974c3051b8f25a1b3419356aa52bf8
LSchultebraucks <L.Schultebraucks@t-online.de>
2018-04-22 20:43:56 +0200

    added CenturyDatasetBuilder

--- 2. Play Money Ball/dataset_builders/century_dataset_builder.py
+++ 2. Play Money Ball/dataset_builders/century_dataset_builder.py
@@ -5,7 +5,7 @@ import pandas as pd
 from .dataset_builder import DataSetBuilder
 
 
-class MaxYearsDataSetBuilder(DataSetBuilder):
+class CenturyDataSetBuilder(DataSetBuilder):
 
     def __init__(self):
         super().__init__()
@@ -13,11 +13,14 @@ class MaxYearsDataSetBuilder(DataSetBuilder):
     def build_data_set(self):
         df = pd.DataFrame()
         for division in self.DIVISIONS:
-            for season_year in range(self.MIN_YEAR, self.MAX_YEAR - 1):
+            for season_year in range(2000, self.MAX_YEAR - 1):
                 path = os.path.join(os.path.dirname(__file__),
                                     "../data/" + division + "_" + str(season_year) + "_" + str(
                                         season_year + 1) + ".csv")
                 df_tmp = pd.read_csv(path, keep_default_na=False, na_values="")
                 df = pd.concat([df, df_tmp], ignore_index=True)
-        df = df[["Div", "Date", "HomeTeam", "AwayTeam", "FTHG", "FTAG", "FTR"]]
+        df = df[["Div", "Date", "HomeTeam", "AwayTeam", "FTHG", "FTAG", "FTR", "HTHG", "HTAG", "HTR", "Attendance",
+                 "Referee", "HS", "AS", "HST", "AST", "HHW", "AHW", "HC", "AC", "HF", "AF", "HO", "AO", "HY", "AY",
+                 "HR", "AR", "HBP", "ABP", "GBH", "GBD", "GBA",
+                 "IWH", "IWD", "IWA", "LBH", "LBD", "LBA", "SBH", "SBD", "SBA", "WHH", "WHD", "WHA"]]
         return df
